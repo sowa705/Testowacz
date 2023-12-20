252835c802a0507e84b2da303e0af4bcc950a9b9
Łukasz Sowa <lukasz.sowa@allegropay.pl>
2023-12-20 10:37:17 +0100

    first commit

---
+++ Testowacz/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await TestPostgres();
+
+app.Run();
+
+
+async Task TestPostgres()
+{
+    var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING");
+
+    var client = new Npgsql.NpgsqlConnection(connectionString);
+
+    client.Open();
+
+    Console.WriteLine("Connected to Postgres");
+
+    // create table
+    var createTableCommand = client.CreateCommand();
+    createTableCommand.CommandText = "CREATE TABLE IF NOT EXISTS test_table (id serial PRIMARY KEY, iter integer NOT NULL)";
+    await createTableCommand.ExecuteNonQueryAsync();
+
+    var sw = new Stopwatch();
+
+    var iteration = 0;
+    while (true)
+    {
+        iteration++;
+        await Task.Delay(1000);
+
+        if (Console.KeyAvailable)
+        {
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Q)
+            {
+                break;
+            }
+        }
+
+        try
+        {
+            sw.Restart();
+            Console.WriteLine($"Iteration: {iteration}");
+
+            // insert data
+            var insertCommand = client.CreateCommand();
+            insertCommand.CommandText = $"INSERT INTO test_table (iter) VALUES ({iteration})";
+            await insertCommand.ExecuteNonQueryAsync();
+
+            // select data
+            var selectCommand = client.CreateCommand();
+            selectCommand.CommandText = $"SELECT * FROM test_table ORDER BY name DESC LIMIT 1";
+            var reader = await selectCommand.ExecuteReaderAsync();
+
+            // verify data
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var iter = reader.GetInt32(1);
+                Console.WriteLine($"id: {id}, iter: {iter}");
+
+                if (iter != iteration)
+                {
+                    throw new Exception($"Invalid iter value: {iter}");
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine($"Iteration {iteration} completed in {sw.Elapsed.TotalMilliseconds:00.00} ms");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+        }
+    }
+}
